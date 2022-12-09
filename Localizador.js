169dd44e3365037342939418f85c8f4ae4532cd1
vinicius-dhenner <vinicius-dhenner@gail.com>
2022-12-08 23:22:25 -0400

    Final-commit

--- Localizador.js
+++ Localizador.js
@@ -6,28 +6,17 @@ const botao = document.querySelector('#button');
 const input = document.querySelector('#perfil')
 const inputConsultarPerfil = input.user;
 
-inputConsultarPerfil.addEventListener('submit', function(event) {
+botao.addEventListener('click', async function(event) {
     event.preventDefault();   
     consultarPerfil(inputConsultarPerfil.value);
-    loader(true);
 })
 
 async function consultarPerfil (perfil) {
-    let response = await fetch(`https://api.github.com/users/${perfil}`)
+    let url = `https://api.github.com/users/${perfil}`
+    let response = await fetch(url)
     let dadosPerfis = await response.json();
     caixa.innerHTML = `
-        <img>${dadosPerfis.avatar_url}</img>
+        <img src="${dadosPerfis.avatar_url}"></img>
         <p>Perfil: ${dadosPerfis.login}</p>
     `
-    loader(false);
-}
-
-function loader (ativo) {
-        if (ativo) { 
-          botao.setAttribute('aria-busy', 'true');
-          botao.textContent = 'Consultando perfis...';
-        } else {
-          botao.removeAttribute('aria-busy'); 
-          botao.textContent = 'Consultar' ;
-        }
 }
